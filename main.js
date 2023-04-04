1bdf47cd8e75e2eafa78e0d2ac1be5b0f18ac4d5
Docky44 <simonpierre643@gmail.com>
2023-04-04 23:56:17 +0200

    Add : template timeline

--- main.js
+++ main.js
@@ -28,3 +28,29 @@ document.addEventListener('click', (event) => {
         menuToggle.querySelector('input').checked = false;
     }
 });
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller()
+
+allBoxes.forEach(box => {
+
+    for(i = 0; i < allRonds.length; i++){
+
+        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+
+            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+
+            let scene = new ScrollMagic.Scene({
+                triggerElement: allRonds[i],
+                reverse: false
+            })
+                .setTween(tween)
+                // .addIndicators()
+                .addTo(controller)
+
+        }
+
+    }
+
+})
